3b41335d96e47dfe1fb7fab5897258aa828e38a9
krishkp17 <krishkumar72309@gmail.com>
2026-09-17 19:02:00 +0530

    Create a folder client and server and create react

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-2xl text-center'>
+      SwordByte
+    </div>
+  )
+}
+
+export default App

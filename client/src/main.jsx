3e1396015197930b57eed012c6f16207ec4e4647
Guddu kumar  Pandey <tumhara-github-email@example.com>
2026-09-18 04:23:06 +0530

    edit index.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
-import "./App.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

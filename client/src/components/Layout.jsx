58ed8a5286748aeeac56399736064a1e39cb0503
krishkp17 <krishkumar72309@gmail.com>
2026-09-18 10:59:44 +0530

    Update client/src/component Layout.jsx resolve some issue

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import React from "react";
-
+import { useState } from "react";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
@@ -8,12 +7,16 @@ export default function Layout({ children }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="min-h-screen">
-      <Sidebar open={open} onClose={() => setOpen(false)} />
+    <div>
+      <Sidebar
+        open={open}
+        onClose={() => setOpen(false)}
+      />
 
       <div className="lg:pl-72">
         <Topbar onMenu={() => setOpen(true)} />
-        <main className="mx-auto max-w-[1500px] px-4 py-6 sm:px-6 lg:px-8 lg:py-8">
+
+        <main className="px-4 sm:px-6 lg:px-8">
           {children}
         </main>
       </div>
